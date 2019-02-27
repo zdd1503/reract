32a9732b617bbc26a45b77fefa2acf4033b51194
赵丹丹 <zdd@jusfoun.com>
2019-02-27 17:06:18 +0800

    feat:提交代码

--- my-app/src/component/redux/toodolist.js
+++ my-app/src/component/redux/toodolist.js
@@ -1,70 +1,83 @@
+/**redux----案例 */
+/**
+ * store就是保存数据的地方，你可以把它看成一个数据，整个应用智能有一个store;
+ * Redux提供createStore这个函数，用来生成Store;
+ * Redux规定一个state对应一个View,只要state相同，view就是一样的，反过来也是一样的，可以通过     store.getState( )获取;
+ * 在Redux中提供了一个对象来告诉Store需要改变state。Action是一个对象其中type属性是必须的，表示Action的名称，
+ * store.dispatch( )是view发出Action的唯一办法;
+ * Reducer是一个纯函数，他接收Action和当前state作为参数，返回一个新的state;
+ */
 import React from 'react';
 import { createStore } from 'redux';
 class Redux extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputVal: ''
-    }
+
   }
   add() {
     const me = this;
-    console.log(me)
-    // store.dispatch({ type: 'INCREMENT' })
+    me.store.dispatch({ type: 'INCREMENT' });
   }
   delete() {
-    console.log(666)
+    const me = this;
+    me.store.dispatch({ type: 'DECREMENT' });
   }
-
-  render() {
-    return (
-      <div>
-        <p>
-          Clicked: <span ref={'value'}>0</span> times
-         <button style={{
-            width: 20,
-            height: 20
-          }} onClick={this.add.bind(this)}>+</button>
-          <button style={{
-            width: 20,
-            height: 20,
-            marginLeft: 30
-          }} onClick={this.delete.bind(this)}>-</button>
-
-        </p>
-      </div>
-    )
+  /**createStore封装----start */
+  createStore() {
+    const me = this;
+    let state;
+    let listeners = [];
+    let getState = () => state;//获取state
+    let dispatch = (action) => { //store.dispatch( )是view发出Action的唯一办法
+      state = me.reducer(state, action); //reducer返回新的state
+      listeners.forEach(l => l())
+    }
+    let subscribe = (listener => {
+      listeners.push(listener)
+      return () => {
+        listeners = listeners.filter((l) => l != listener)
+      }
+    })
+    dispatch()
+    return {
+      getState,
+      dispatch,
+      subscribe
+    }
   }
-  counter(state = 0, action) {
+  /**createStore封装----end */
+  reducer(state = 0, action) {
+    if (!action) return state;
     switch (action.type) {
       case 'INCREMENT':
         return state + 1
-      case "DECREMENT":
+      case 'DECREMENT':
         return state - 1
       default:
         return state
     }
   }
-  renderd() {
-    const me = this;
-    let store = createStore(me.counter)
-    me.refs.value.innerHTML = store.getState().toString()
+  render() {
+    return (
+      <div>
+        <button onClick={this.add.bind(this)} style={{ width: 20, height: 20 }}>+</button>
+        <input type='text' placeholder='请输入' ref='input' style={{
+          width: 40, marginLeft: 5, height: 20,
+          border: '2px solid blue', textAlign: 'center',
+          color: 'red',
+          fontSize: 12
+        }} />
+        <button onClick={this.delete.bind(this)} style={{ width: 20, height: 20, marginLeft: 5 }}>-</button>
+      </div>
+    )
   }
-
   componentDidMount() {
     const me = this;
-    me.renderd()
-    let store = createStore(me.counter)
-    console.log(store)
-
-    store.subscribe(me.renderd);
-
-    // store.dispatch({ type: 'INCREMENT' });
-    // store.dispatch({ type: 'INCREMENT' })
-    // store.dispatch({ type: 'DECREMENT' })
-  }
-  componentDidUpdate() {
-
+    me.store = createStore(me.reducer);
+    me.store.subscribe(function () {
+      me.refs.input.value = me.store.getState();
+    });
   }
 }
+
 export default Redux;
